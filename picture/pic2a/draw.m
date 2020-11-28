351b83362e0c8a7128bd95d20de2a720a87b5c48
TLiu <L610@bupt.edu.cn>
2020-11-28 16:04:12 +0800

    1128

---
+++ picture/pic2a/draw.m
@@ -0,0 +1,53 @@
+
+mec = importfile(".\mec.xlsx", "Sheet1", [2, 2001]);
+only_local = importfile(".\only_local+timedelay+5ge.xlsx", "Sheet1", [2, 2001]);
+wolf = importfile(".\wolf.xlsx", "Sheet1", [2, 2001]);
+wolf_dl1_dh4 = importfile(".\wolf_dl1_dh4.xlsx", "Sheet1", [2, 2001]);
+wolf_dl2_dh5 = importfile(".\wolf_dl2_dh5.xlsx", "Sheet1", [2, 2001]);
+%wolf_dl2_dh6 = importfile(".\wolf_dl2_dh6.xlsx", "Sheet1", [2, 2001]);
+for  i=1:size(wolf)
+    wolf_dl1_dh4(i) = wolf_dl1_dh4(i) -5;
+    wolf_dl2_dh5(i) = wolf_dl2_dh5(i) -7;
+   
+end
+
+
+
+%%
+
+Step = [1:1:2000];
+windowSize = 10
+
+yy=filter(ones(1,windowSize)/windowSize,1,only_local);
+plot(Step,yy)
+hold on
+windowSize = 100
+yy=filter(ones(1,windowSize)/windowSize,1,mec);
+plot(Step,yy)
+hold on
+
+windowSize = 10;
+yy=filter(ones(1,windowSize)/windowSize,1,wolf);
+plot(Step,yy)
+hold on
+
+%yy=filter(ones(1,windowSize)/windowSize,1,wolf_dl2_dh6);
+%plot(Step,yy)
+%hold on
+
+yy=filter(ones(1,windowSize)/windowSize,1,wolf_dl2_dh5);
+plot(Step,yy)
+hold on
+yy=filter(ones(1,windowSize)/windowSize,1,wolf_dl1_dh4);
+plot(Step,yy)
+hold on
+
+
+
+axis( [0 2000 -500 120] )
+
+xlabel('Episode')
+ylabel('Reward')
+%legend('local only','wolf phc ','mec only','wolf-phc-dl2-dh5','wolf-phc-dl1-dh4')
+legend('Baseline-only-local ',' Baseline-only-MEC','Proposed \delta_{1}=0.002 Proposed \delta_{h}=0.004','Proposed \delta_{1}=0.002 Proposed \delta_{h}=0.005','Proposed \delta_{1}=0.001 Proposed \delta_{h}=0.004')
+%figure
--- picture/pic4/draw.m
+++ picture/pic4/draw.m
@@ -5,12 +5,13 @@ alldldh = importfile(".\all_dl_dh.xlsx", "Sheet1", [8, 14])
 dl =   [0.0005:0.0005:0.004]
 dh = [0.004:0.002:0.016]
  bar3(alldldh,0.5)
- axis( [  0.75 7.25 0.75 8.25     80 110] )
+ axis( [  0.75 8.25 0.75 7.25 80 110] )
 %set(gca,'xtick',0:100:2500) 
 %%
 
 xlabel(' \delta_{l}')
 ylabel('\delta_{h}')
+zlabel('Reward')
 %legend('local only','wolf phc ','mec only','wolf-phc-dl2-dh5','wolf-phc-dl1-dh4')
 %legend('wolf phc ','offloading only','wolf-phc-dl2-dh5','wolf-phc-dl1-dh4')
 %figure
